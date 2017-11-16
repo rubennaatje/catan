afc62d62e1f7aa389a18d2ef8a1bf7b722112d79
ltadeurs <luukvandeursen@gmail.com>
2017-11-16 16:02:40 +0100

    Splashscreen toegevoegd

--- src/LoginView/Main.java
+++ src/LoginView/Main.java
@@ -1,5 +1,7 @@
 package LoginView;
 
+import java.net.MalformedURLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -12,12 +14,24 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage)
 	{
+		//Load spashscreen
+		SplashScreen Spl = new SplashScreen();
+		
+		//Check if Splashscreen is done
+		boolean ready = Spl.getDoneLoading();
+		while(ready == false) {
+			ready = Spl.getDoneLoading();
+			System.out.println("loading");
+		}
+		
+		//Load loginView
 		LoginView loginview = new LoginView();
 		loginview.laucnhview(primaryStage);
 	}
 
 	public static void main(String[] args)
 	{
+		//start the application
 		launch(args);
 	}
 }
