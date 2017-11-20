f155b4708763716d23b833554b9391f7392016e7
Wouter Thiesen <wwjthies@avans.nl>
2017-11-20 15:07:34 +0100

    dice klasse

--- src/model/Dice.java
+++ src/model/Dice.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.util.Random;
+
 public class Dice {
 
+	private int diceThrow;
+	private Random random = new Random();
+	
+	public Dice() {
+		
+	}
+	
+	private void throwDice() {
+		diceThrow = random.nextInt(6 + 1);
+	}
+	
+	public int getThrow() {
+		return diceThrow;
+	}
 }
