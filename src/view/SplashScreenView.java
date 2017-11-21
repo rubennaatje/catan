3cd827bd3a7d5e9752d8dd50b844b87c0df3cf69
ltadeurs <luukvandeursen@gmail.com>
2017-11-21 13:14:49 +0100

    Splashscreen is nu gemaakt in JavaFX

--- src/view/SplashScreenView.java
+++ src/view/SplashScreenView.java
@@ -1,12 +1,38 @@
 package view;
 
+import java.io.IOException;
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-import view.javaFXTemplates.PaneTemplate;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
-public class SplashScreenView extends PaneTemplate {
-	
-	public SplashScreenView(Stage stage) {
-		super(SplashScreenView.class.getResource("fxml/SplashScreenView.fxml"), stage);
+public class SplashScreenView
+{
+	protected void initUI(Stage primaryStage)
+	{
+		Parent root;
+		try
+		{
+			root = FXMLLoader.load(getClass().getResource("SplashScreenView.fxml"));
+			Scene scene = new Scene(root);
+
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Splash Screen");
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			primaryStage.show();
+
+			PauseTransition pause = new PauseTransition(Duration.seconds(3));
+			pause.setOnFinished(e -> {
+				primaryStage.hide();
+			});
+			pause.play();
+
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
 }
