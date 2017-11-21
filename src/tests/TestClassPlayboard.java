cef8e881a56d2e96f4eed5cb7e30513150022881
Stijn Smits <stijn@stijn98s.nl>
2017-11-21 14:26:11 +0100

    merge

--- src/tests/TestClassPlayboard.java
+++ src/tests/TestClassPlayboard.java
@@ -17,12 +17,7 @@ public class TestClassPlayboard extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		PlayBoardView playboardview = new PlayBoardView(primaryStage);
-<<<<<<< Upstream, based on origin/luuk
 		
-		
-=======
-
->>>>>>> 04398fc added event as parameter to pieces
 		Location locs = new Location(1000);
 		
 		
@@ -34,30 +29,6 @@ public class TestClassPlayboard extends Application {
 			};
 		};
 
-<<<<<<< Upstream, based on origin/luuk
-		playboardview.addHex(locs.getHexEdges(2,4), locs.getCoordinate(2,4), 0, "hout");
-		playboardview.addHex(locs.getHexEdges(3,6), locs.getCoordinate(3,6), 0, "hout");
-		playboardview.addHex(locs.getHexEdges(4,8), locs.getCoordinate(4,8), 0, "wol");
-		playboardview.addHex(locs.getHexEdges(3,3), locs.getCoordinate(3,3), 0, "graan");
-		playboardview.addHex(locs.getHexEdges(4,5), locs.getCoordinate(4,5), 0, "baksteen");
-		playboardview.addHex(locs.getHexEdges(5,7), locs.getCoordinate(5,7), 0, "baksteen");
-		playboardview.addHex(locs.getHexEdges(6,9), locs.getCoordinate(6,9), 0, "erts");
-		playboardview.addHex(locs.getHexEdges(4,2), locs.getCoordinate(4,2), 0, "baksteen");
-		playboardview.addHex(locs.getHexEdges(5,4), locs.getCoordinate(5,4), 0, "erts");
-		playboardview.addHex(locs.getHexEdges(6,6), locs.getCoordinate(6,6), 0, "woestijn");
-		playboardview.addHex(locs.getHexEdges(7,8), locs.getCoordinate(7,8), 3, "wol");
-		playboardview.addHex(locs.getHexEdges(8,10), locs.getCoordinate(8,10), 3, "hout");
-		playboardview.addHex(locs.getHexEdges(6,3), locs.getCoordinate(6,3), 9, "graan");
-		playboardview.addHex(locs.getHexEdges(7,5), locs.getCoordinate(7,5), 3, "baksteen");
-		playboardview.addHex(locs.getHexEdges(8,7), locs.getCoordinate(8,7), 2, "graan");
-		playboardview.addHex(locs.getHexEdges(9,9), locs.getCoordinate(9,9), 1, "wol");
-		playboardview.addHex(locs.getHexEdges(8,4), locs.getCoordinate(8,4), 9, "hout");
-		playboardview.addHex(locs.getHexEdges(9,6), locs.getCoordinate(9,6), 2, "wol");
-		playboardview.addHex(locs.getHexEdges(10,8), locs.getCoordinate(10,8), 0, "baksteen");
-		
-		playboardview.addRobber(locs.getCoordinate(6,6));
-		
-=======
 		playboardview.addHex(locs.getHexEdges(2, 4), locs.getCoordinate(2, 4), 0, "hout");
 		playboardview.addHex(locs.getHexEdges(3, 6), locs.getCoordinate(3, 6), 0, "hout");
 		playboardview.addHex(locs.getHexEdges(4, 8), locs.getCoordinate(4, 8), 0, "wol");
@@ -83,15 +54,12 @@ public class TestClassPlayboard extends Application {
 		playboardview.addPiece(locs.getCoordinate(2, 5), "player_blue", event);
 		playboardview.addRobber(locs.getCoordinate(6, 6));
 
->>>>>>> 04398fc added event as parameter to pieces
 		playboardview.show();
-<<<<<<< Upstream, based on origin/luuk
 	//new ChallengeView(primaryStage).show();
 		//new PlayerView(primaryStage).show();
 		//new SplashScreenView(primaryStage).show();
 		
 		
-=======
 		// new ChallengeView(primaryStage).show();
 		// new PlayerView(primaryStage).show();
 		// new SplashScreenView(primaryStage).show();
@@ -100,7 +68,6 @@ public class TestClassPlayboard extends Application {
 		// DatabaseManager.connect();
 		// board.createBoard(40);
 		// DatabaseManager.disconnect();
->>>>>>> 04398fc added event as parameter to pieces
 	}
 
 }
