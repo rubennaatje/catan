c6806da58bbdc678cb706ae8eafd98a5e8b66999
Geordi Trapman <grtrapma@avans.nl>
2017-12-06 00:47:43 +0100

    Working on trade and robber functions

--- src/model/TradeHelper.java
+++ src/model/TradeHelper.java
@@ -5,17 +5,17 @@ import java.util.HashMap;
 //handles logic for trade, will be done later
 public class TradeHelper {
 	private TradeHelper() {
-		
+
 	}
 
-	public static void registerTrade(String spelId, PlayerModel player, HashMap<String, Integer>[] tradeSuggestion) {
-		//for testing
-		for (HashMap<String, Integer> hashMap : tradeSuggestion) {
-			System.out.println(hashMap.get("Hout"));
-			System.out.println(hashMap.get("Wol"));
-			System.out.println(hashMap.get("Graan"));
-			System.out.println(hashMap.get("Baksteen"));
-			System.out.println(hashMap.get("Erts"));
+	public static void registerTrade(String spelId, PlayerModel player, HashMap<TileType, Integer>[] tradeSuggestion) {
+		// for testing
+		for (HashMap<TileType, Integer> hashMap : tradeSuggestion) {
+			System.out.println(hashMap.get(TileType.H));
+			System.out.println(hashMap.get(TileType.W));
+			System.out.println(hashMap.get(TileType.G));
+			System.out.println(hashMap.get(TileType.B));
+			System.out.println(hashMap.get(TileType.E));
 		}
 	}
 }
