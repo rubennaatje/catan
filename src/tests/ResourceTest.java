f15ed68c5d1786adcf356a02c0aacdbaafb88ad2
Marquatoes <markdonker319@hotmail.com>
2017-12-07 18:05:01 +0100

    commited

---
+++ src/tests/ResourceTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import controller.DatabaseManager;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import model.PlayerModel;
+import model.PlayerUser;
+import view.PlayerView;
+
+public class ResourceTest extends Application
+{
+	public static void main(String[] args) {
+		DatabaseManager.connect();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			DatabaseManager.disconnect();
+			System.out.println("databasemanager shut down");
+		}));
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		PlayerView playerView = new PlayerView();
+		PlayerModel playerModel = new PlayerUser("bart", "770");
+		playerModel.addObserver(playerView);
+		
+	}
+}
