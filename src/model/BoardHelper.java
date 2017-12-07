e28e650c73964c9b7a6daff788eaa2bbbc449d62
Kaasfonthijn <tajlinde@avans.nl>
2017-12-07 18:04:04 +0100

    finished getlargest army method

--- src/model/BoardHelper.java
+++ src/model/BoardHelper.java
@@ -131,6 +131,7 @@ public class BoardHelper {
 			}
 			
 			if(iSize == 1 && iAmount > 2) {
+				DatabaseManager.createStatement().executeUpdate("INSERT INTO spel (grootste_rm_username) VALUES	 ('"+sNewLargestArmy+"')");
 				return sNewLargestArmy;
 			}
 		}
