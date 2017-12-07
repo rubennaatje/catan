ffdf66c89d8eb7f363e53425d26d64009be0c77f
Wouter Thiesen <wwjthies@avans.nl>
2017-12-07 16:31:43 +0100

    updated developcardcontroller, includes method to refresh development cards

--- src/controller/DevelopCardController.java
+++ src/controller/DevelopCardController.java
@@ -14,22 +14,20 @@ import model.VictoryPointCard;
 public class DevelopCardController {
 	private Random random = new Random();
 	private int cardint;
-	private ArrayList<String> cardsUnUsed = new ArrayList<>(); //list of drawn cards
 	private String cardId;
 	private String kaarttype;
 	private String kaartnaam;
 	private int index = 1;
 	private String spelId;
 	private String uitleg;
-	private HashMap<String, DevelopmentCard> cardList = new HashMap<>(); //hashmap with card objects assigned to players, cardId as key
+	private ArrayList<DevelopmentCard> list = new ArrayList<>();
 	
 	public DevelopCardController(String spelId) {
 		this.spelId = spelId;
 	}
 	
 	public void givePlayerCard(String username) {
-		boolean gavecard = false;
-		while(!gavecard) {
+			ArrayList<String> cardsUnUsed = new ArrayList<>();
 			try { // create list of unused cards
 				ResultSet cardsNotUsed = DatabaseManager.createStatement().executeQuery("SELECT idontwikkelingskaart FROM spelerontwikkelingskaart WHERE username IS NULL AND idspel = "+ spelId +";");
 				while(cardsNotUsed.next()) {
@@ -43,7 +41,31 @@ public class DevelopCardController {
 			cardId = cardsUnUsed.get(cardint);
 			cardsUnUsed.remove(cardint);
 			index = 1;
-			try {
+			assignCard(cardId, username);
+	}
+	
+//	public void playCard(String cardId) {
+//		
+//	}
+	
+	public void refreshDevCards(String username) { // creates devcards objects for player
+		try {
+			ResultSet cardsUsed = DatabaseManager.createStatement().executeQuery("SELECT idontwikkelingskaart FROM spelerontwikkelingskaart WHERE username = '"+username+"' AND idspel = "+ spelId +" AND gespeeld = 0;");
+			while(cardsUsed.next()) {
+				assignCard(cardsUsed.getString("idontwikkelingskaart"), username);
+				System.out.println(cardsUsed.getString("idontwikkelingskaart"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	public ArrayList<DevelopmentCard> getPlayerCards(){ //returns arraylist of playercards
+		return list;
+	}
+	
+	private void assignCard(String cardId, String username) { //method to assign devcards to player
+		try {
 			ResultSet kaarttypes = DatabaseManager.createStatement().executeQuery("SELECT o.naam, k.type, k.uitleg FROM ontwikkelingskaart o JOIN kaarttype k ON o.naam = k.naam WHERE o.idontwikkelingskaart = '"+ cardId+ "';");
 				if(kaarttypes.first()) {
 				kaartnaam = kaarttypes.getString(1);
@@ -56,26 +78,19 @@ public class DevelopCardController {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-				switch(kaarttype) { //create card object
-					case "ridder":
-						cardList.put(cardId, new KnightCard(spelId , cardId, username, kaarttype));
-						break;
-					case "gebouw":
-						cardList.put(cardId, new ProgressCard(spelId, cardId, username, kaarttype, kaartnaam, uitleg));
-						break;
-					case "vooruitgang":
-						cardList.put(cardId, new VictoryPointCard(spelId, cardId, username, kaarttype, kaartnaam));
-						break;
-					}
-				gavecard = true; //cancel loop if card is given
-			}
-	}
-	
-	public void playCard(String cardId) {
-		
-	}
-	
-	
 
+				switch(kaarttype) { //create card object
+				case "ridder":
+					list.add(new KnightCard(spelId , cardId, username, kaarttype, kaartnaam));
+					break;
+				case "gebouw":
+					list.add(new ProgressCard(spelId, cardId, username, kaarttype, kaartnaam, uitleg));
+					break;
+				case "vooruitgang":
+					list.add(new VictoryPointCard(spelId, cardId, username, kaarttype, kaartnaam));
+					break;
+				}
+				
+	}
 
 }
