d45f8307df12e8393fee25f6727d3e526c13d588
Marquatoes <markdonker319@hotmail.com>
2017-12-07 17:32:11 +0100

    PlayerScreens

--- src/view/PlayerView.java
+++ src/view/PlayerView.java
@@ -4,8 +4,8 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import model.PlayerModel;
 import view.javaFXTemplates.PaneTemplate;
 
@@ -13,7 +13,7 @@ public class PlayerView extends PaneTemplate implements Observer {
 	
 	  	@FXML private Text cards;
 	    @FXML private Text devcards;
-	    @FXML private Text knights;
+	    @FXML private Text knight;
 	    @FXML private Text score;
 	    @FXML private Text road;
 		
@@ -29,9 +29,22 @@ public class PlayerView extends PaneTemplate implements Observer {
 		PlayerModel playerData = (PlayerModel)arg0;
 		cards.setText(playerData.getCards());
 		devcards.setText(playerData.getDevCards());
-		knights.setText(playerData.getKnights());
+		knight.setText(playerData.getKnights());
 		score.setText(playerData.getScore());
 		road.setText(playerData.getRoad());
+		
+		switch(playerData.getPlayerNumber())
+		{
+		case 1:  getStyleClass().add("playerRed");
+			break;
+		case 2: getStyleClass().add("playerBlue");
+			break;
+		case 3: getStyleClass().add("playerOrange");
+			break;
+		case 4:getStyleClass().add("playerWhite");
+			break;
+		
+		}
 	}	
 	
 
