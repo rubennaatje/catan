c432b05e4a740a5b668fe94331e84e45b5790793
Stijn Smits <stijn@stijn98s.nl>
2017-12-11 12:23:07 +0100

    changed database

--- src/controller/DatabaseManager.java
+++ src/controller/DatabaseManager.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public final class DatabaseManager {
 	
-	private final static String sUrl = "jdbc:mysql://databases.aii.avans.nl/";    
+	private final static String sUrl = "jdbc:mysql://projcatan.westeurope.cloudapp.azure.com/";    
     private final static String sDriverName = "com.mysql.jdbc.Driver";
     private static Connection connection;
     private static Statement statement;
@@ -19,7 +19,7 @@ public final class DatabaseManager {
 			System.out.println("Driver not found");
 		}
     	try {
-    		connection = DriverManager.getConnection(sUrl + "tajlinde_db", "tajlinde", "Vtb1avans");
+    		connection = DriverManager.getConnection(sUrl + "catan", "tajlinde", "Vtb1avans");
     		statement = connection.createStatement();
 		} catch (SQLException e) {
 			System.out.println("Could not open connection");
