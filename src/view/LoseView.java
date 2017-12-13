a5bf472bb71295168777928a0abeabfe0eeef6c2
Kim <kimhermsen@hotmail.nl>
2017-12-13 15:10:02 +0100

    win/lose

---
+++ src/view/LoseView.java
@@ -0,0 +1,28 @@
+package view;
+
+import controller.CatanController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import view.javaFXTemplates.PaneTemplate;
+
+public class LoseView extends PaneTemplate {
+	
+	private CatanController controller; 
+	@FXML private Button btnBack;
+	
+	public LoseView(Stage stage, CatanController controller) {
+		super(LoseView.class.getResource("fxml/LoseView.fxml"), stage);
+		this.controller =  controller;
+		
+		btnBack.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				controller.openMenuScreen();
+			}
+		});
+	}
+}
