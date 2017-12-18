716737143a4a28f7f4a2c230433a98a0f810fae5
Ruben Soerdien <ruben.soerdien@hotmail.com>
2017-12-18 13:30:54 +0100

    set db back

--- src/controller/DatabaseManager.java
+++ src/controller/DatabaseManager.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public final class DatabaseManager {
 	
-	private final static String sUrl = "jdbc:mysql://databases.aii.avans.nl/";    
+	private final static String sUrl = "jdbc:mysql://projcatan.westeurope.cloudapp.azure.com/";    
     private final static String sDriverName = "com.mysql.jdbc.Driver";
     private static Connection connection;
     private static Statement statement;
@@ -19,7 +19,7 @@ public final class DatabaseManager {
 			System.out.println("Driver not found");
 		}
     	try {
-    		connection = DriverManager.getConnection(sUrl + "2017_vsoprj2_catan_abcd", "42IN02VTSOb", "bedrijfsauto");
+    		connection = DriverManager.getConnection(sUrl + "catan", "tajlinde", "Vtb1avans");
     		statement = connection.createStatement();
 		} catch (SQLException e) {
 			System.out.println("Could not open connection");
@@ -39,12 +39,8 @@ public final class DatabaseManager {
 			System.out.println("Could not close connection");
 		}
     }
+    
     public static Statement createStatement() throws SQLException {
 		return connection.createStatement();
 	}
-    //this produces funky errors, use createStatement instead ref: https://stackoverflow.com/questions/5840866/getting-java-sql-sqlexception-operation-not-allowed-after-resultset-closed
-    @Deprecated
-    public static Statement getStatement() {
-    	return statement;
-    }
 }
